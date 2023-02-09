81f420d1d04ffe520e26d8113cfeb3e1594087d2
Vgram2 <124300591+Vgram2@users.noreply.github.com>
2023-02-09 22:48:11 +0200

    Update game-code.js

--- js/game-code.js
+++ js/game-code.js
@@ -491,7 +491,7 @@
 			textSize(30);
 			text("Score: " + asteroidSwarm.asteroidsPassed, 30, 135); // Score is shown
 			
-			text("Rocks: " + spaceship.missiles, 1120, 60); // Score is shown
+			text("Rock: " + spaceship.missiles, 1120, 60); // Score is shown
 		
 			if (!startGame)
 				rect(280, 280, 680, 140); // rectangle (window) to show the message to start game
